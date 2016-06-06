2cd86e91e28b8993a01d681ffd934879969515f2
adrpadua <adrpadua@gmail.com>
2016-06-06 01:47:47 -0700

    Added Data model to the CoreData thingy

---
+++ CharacterObject+CoreDataProperties.swift
@@ -0,0 +1,19 @@
+//
+//  CharacterObject+CoreDataProperties.swift
+//  
+//
+//  Created by Adrian Padua on 6/6/16.
+//
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+import Foundation
+import CoreData
+
+extension CharacterObject {
+
+    @NSManaged var characterData: NSObject?
+
+}
---
+++ CharacterObject.swift
@@ -0,0 +1,17 @@
+//
+//  CharacterObject.swift
+//  
+//
+//  Created by Adrian Padua on 6/6/16.
+//
+//
+
+import Foundation
+import CoreData
+
+
+class CharacterObject: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
