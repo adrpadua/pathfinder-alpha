43f9e939acd0ba742fa3cec3ef4db055e6121cf6
adrpadua <adrpadua@gmail.com>
2016-06-12 16:31:58 -0700

    Before changes

---
+++ pathfinder-alpha/SkillsVC.swift
@@ -0,0 +1,31 @@
+//
+//  SkillsVC.swift
+//  pathfinder-alpha
+//
+//  Created by Adrian Padua on 6/11/16.
+//  Copyright © 2016 Adrian Padua. All rights reserved.
+//
+
+import UIKit
+
+class SkillsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
