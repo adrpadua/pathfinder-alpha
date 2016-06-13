1203068d2eeb79fbc8b2c3b7bb0e84c062e5b335
adrpadua <adrpadua@gmail.com>
2016-06-13 01:50:55 -0700

    Each skill has their own list of favored classes

--- pathfinder-alpha/Classes.swift
+++ pathfinder-alpha/Classes.swift
@@ -16,73 +16,6 @@ enum Classes: String {
         return self.rawValue
     }
     
-    func classSkills() -> [String] {
-        var array = [String]()
-        
-        switch self {
-        case .Cleric:
-            array.append(Skills.Acrobatics.name())
-            array.append(Skills.Climb.name())
-            array.append(Skills.Diplomacy.name())
-            array.append(Skills.Heal.name())
-            array.append(Skills.KnowledgeArcana.name())
-            array.append(Skills.KnowledgeHistory.name())
-            array.append(Skills.KnowledgeNobility.name())
-            array.append(Skills.KnowledgePlanes.name())
-            array.append(Skills.KnowledgeReligion.name())
-            array.append(Skills.Linguistics.name())
-            array.append(Skills.Profession.name())
-            array.append(Skills.SenseMotive.name())
-            array.append(Skills.Spellcraft.name())
-            return array
-        case .Fighter:
-            array.append(Skills.Acrobatics.name())
-            array.append(Skills.Climb.name())
-            array.append(Skills.Diplomacy.name())
-            array.append(Skills.Heal.name())
-            array.append(Skills.KnowledgeArcana.name())
-            array.append(Skills.KnowledgeHistory.name())
-            array.append(Skills.KnowledgeNobility.name())
-            array.append(Skills.KnowledgePlanes.name())
-            array.append(Skills.KnowledgeReligion.name())
-            array.append(Skills.Linguistics.name())
-            array.append(Skills.Profession.name())
-            array.append(Skills.SenseMotive.name())
-            array.append(Skills.Spellcraft.name())
-            return array
-        case .Rogue:
-            array.append(Skills.Acrobatics.name())
-            array.append(Skills.Climb.name())
-            array.append(Skills.Diplomacy.name())
-            array.append(Skills.Heal.name())
-            array.append(Skills.KnowledgeArcana.name())
-            array.append(Skills.KnowledgeHistory.name())
-            array.append(Skills.KnowledgeNobility.name())
-            array.append(Skills.KnowledgePlanes.name())
-            array.append(Skills.KnowledgeReligion.name())
-            array.append(Skills.Linguistics.name())
-            array.append(Skills.Profession.name())
-            array.append(Skills.SenseMotive.name())
-            array.append(Skills.Spellcraft.name())
-            return array
-        case .Wizard:
-            array.append(Skills.Acrobatics.name())
-            array.append(Skills.Climb.name())
-            array.append(Skills.Diplomacy.name())
-            array.append(Skills.Heal.name())
-            array.append(Skills.KnowledgeArcana.name())
-            array.append(Skills.KnowledgeHistory.name())
-            array.append(Skills.KnowledgeNobility.name())
-            array.append(Skills.KnowledgePlanes.name())
-            array.append(Skills.KnowledgeReligion.name())
-            array.append(Skills.Linguistics.name())
-            array.append(Skills.Profession.name())
-            array.append(Skills.SenseMotive.name())
-            array.append(Skills.Spellcraft.name())
-            return array
-        }
-    }
-    
     func skillRanksPerLevel() -> Int { // + INT Modifier
         switch self {
         case .Cleric:
@@ -95,4 +28,20 @@ enum Classes: String {
             return 2
         }
     }
+    
+    func isSpellcaster() -> Bool {
+        
+        switch self {
+        case .Cleric:
+            return true
+        case .Fighter:
+            return false
+        case .Rogue:
+            return false
+        case .Wizard:
+            return true
+        }
+        
+    }
 }
+
